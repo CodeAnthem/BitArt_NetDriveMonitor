4c7b00589db9bf629572d3b849ef8177eecdc8f5
king averon <averon666@gmail.com>
2021-06-26 21:48:54 +0200

    update

--- UI-Console/Program.cs
+++ UI-Console/Program.cs
@@ -1,4 +1,5 @@
-﻿using BitArt_Network_Helpers;
+﻿using BitArt_Netdrive_Helpers.tests;
+using BitArt_Network_Helpers;
 using NetDriveMonitor;
 using NetDriveMonitor.interfaces;
 using System;
@@ -11,26 +12,48 @@ namespace UI_Console
 	{
 		private static void Main(string[] args)
 		{
+			// ------------------------------------
+			Console.WriteLine("Tests: Started.\n");
+			// ------------------------------------
+
 			//var core = new NDMCore();
 			//var cmd = new ConsoleCmds(core);
 
 			//cmd.Start(); // start the tool
 			//cmd.HoldAndQuit(); // hold screen
 
-			BAHostMonitor monitor = new BAHostMonitor();
+			// ------------------------------------
+			// Testing Net drive mapper
+			// ------------------------------------
+			//HostMonitor monitor = new HostMonitor();
+			//monitor.NotifyOnChangesOnly = false;
+			//monitor.NotifyOnFirstPing = true;
+			//monitor.ScanInterval = 500;
+			//monitor.OnHostChanged += HostUpdated;
 
-			monitor.NotifyOnChangesOnly = false;
-			monitor.NotifyOnFirstPing = true;
-			monitor.ScanInterval = 500;
-			monitor.OnHostChanged += HostUpdated;
+			//monitor.AddHost("dp-nas10");
+			//monitor.AddHost("dp-nas10.dp");
 
-			monitor.AddHost("dp-nas10");
-			monitor.AddHost("dp-nas10.dp");
+			//monitor.Start();
+			//Console.WriteLine("Monitoring {0} targets: ", monitor.HostCount);
+			// ------------------------------------
 
-			monitor.Start();
-			Console.WriteLine("Monitoring {0} targets: ", monitor.HostCount);
+			// ------------------------------------
+			// Testing Net drive mapper
+			// ------------------------------------
+			var drives = NetdriveGenerator.GetDummyData();
+			var test = new NetdriveMapperTest(drives);
+			test.AddDrives();
+			test.RemoveDrives();
+			test.AddDrives();
+			test.RemoveAll();
+			// ------------------------------------
 
+			// ------------------------------------
+			Console.WriteLine("Tests: Completed.");
+			Console.WriteLine("\nPress any key to continue!\n");
 			Console.ReadLine();
+			// ------------------------------------
 		}
 
 		private static void HostUpdated(string hostName, bool state)
